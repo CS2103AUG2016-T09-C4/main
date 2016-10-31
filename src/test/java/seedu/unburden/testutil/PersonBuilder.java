33d5e99e129ca932bcfeb531201e59e878345d9a
YangLuhua <e0012658@u.nus.edu>
2016-10-31 19:50:39 +0800

    Add more methods in PersonBuilder to test commands

--- src/test/java/seedu/unburden/testutil/PersonBuilder.java
+++ src/test/java/seedu/unburden/testutil/PersonBuilder.java
@@ -5,7 +5,8 @@ import seedu.unburden.model.tag.Tag;
 import seedu.unburden.model.task.*;
 
 /**
- *
+ * A class which builds testing tasks
+ * @@author A0147986H
  */
 public class PersonBuilder {
 
@@ -20,13 +21,33 @@ public class PersonBuilder {
         return this;
     }
 
+    public PersonBuilder withDate(String date) throws IllegalValueException {
+        this.person.setDate(new Date(date));
+        return this;
+    }
+
+    public PersonBuilder withStartTime(String startTime) throws IllegalValueException {
+        this.person.setStartTime(new Time(startTime));
+        return this;
+    }
+    
+    public PersonBuilder withEndTime(String endTime) throws IllegalValueException {
+        this.person.setEndTime(new Time(endTime));
+        return this;
+    }
+    
+    public PersonBuilder withTaskDescription(String taskD) throws IllegalValueException {
+        this.person.setTaskDescription(new TaskDescription(taskD));
+        return this;
+    }
+    
     public PersonBuilder withTags(String ... tags) throws IllegalValueException {
         for (String tag: tags) {
             person.getTags().add(new Tag(tag));
         }
         return this;
     }
-
+   
     public TestTask build() {
         return this.person;
     }
