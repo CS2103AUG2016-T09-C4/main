2e6b667a02c638689e91e5c9734600b69248493e
YangLuhua <e0012658@u.nus.edu>
2016-11-01 11:34:53 +0800

    uncommand @test

--- src/test/java/guitests/AddCommandTest.java
+++ src/test/java/guitests/AddCommandTest.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.assertTrue;
 
 public class AddCommandTest extends ListOfTaskGuiTest {
 
-    //@Test
+    @Test
     public void add() {
         //add one task
         TestTask[] currentList = td.getTypicalPersons();
