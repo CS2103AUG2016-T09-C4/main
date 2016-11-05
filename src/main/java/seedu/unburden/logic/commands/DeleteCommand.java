c7f743c605715dd3902f0e5381562a38a44ba973
Yang Luhua <e0012658@u.nus.edu>
2016-11-06 04:38:13 +0800

    Delete command Indentation

--- src/main/java/seedu/unburden/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/unburden/logic/commands/DeleteCommand.java
@@ -16,50 +16,52 @@ import seedu.unburden.model.task.UniqueTaskList.TaskNotFoundException;
  */
 public class DeleteCommand extends Command {
 
-    public static final String COMMAND_WORD = "delete";
+	public static final String COMMAND_WORD = "delete";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the task identified by the index number used in the last task listing.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 2 ";
+	public static final String MESSAGE_USAGE = COMMAND_WORD
+			+ ": Deletes the task identified by the index number used in the last task listing.\n"
+			+ "Parameters: INDEX (must be a positive integer)\n"
+			+ "Example: " + COMMAND_WORD + " 2 ";
 
-    public static final String MESSAGE_DELETE_TASK_SUCCESS = "Deleted Task: %1$s";
+	public static final String MESSAGE_DELETE_TASK_SUCCESS = "Deleted Task: %1$s";
 
-    public final ArrayList<Integer> targetIndexes;
+	public final ArrayList<Integer> targetIndexes;
 
-    public DeleteCommand(ArrayList<Integer> targetIndexes) {
-        this.targetIndexes = targetIndexes;
-    }
+	public DeleteCommand(ArrayList<Integer> targetIndexes) {
+		this.targetIndexes = targetIndexes;
+	}
 
-    /*public DeleteCommand(String targetIndexes) {
+	/*public DeleteCommand(String targetIndexes) {
     	this.targetIndex = 0;
         this.targetIndexes = targetIndexes;
         this.mode = "indexes";
         this.targetIndex_low = 0;
         this.targetIndex_high = 0;
     }
-    */
-    @Override
-    public CommandResult execute() throws DuplicateTagException, IllegalValueException {
+	 */
+	@Override
+	public CommandResult execute() throws DuplicateTagException, IllegalValueException {
 
-        UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
-        for(int i = 0; i < targetIndexes.size(); i++){
-        	
-        if (lastShownList.size() < targetIndexes.get(i)) {
-            indicateAttemptToExecuteIncorrectCommand();
-            return new CommandResult(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
-        }
-        	 ReadOnlyTask taskToDelete = lastShownList.get(targetIndexes.get(i) - i - 1);
+		UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
 
-             try {
-            	model.saveToPrevLists();
-                 model.deleteTask(taskToDelete);
-             } catch (TaskNotFoundException pnfe) {
-                 assert false : "The target task cannot be missing";
-             }
-        }
-             return new CommandResult(String.format(MESSAGE_DELETE_TASK_SUCCESS, targetIndexes));         
-      }          
-   }
+		if (lastShownList.size() < targetIndexes.get(targetIndexes.size()-1)) {
+			indicateAttemptToExecuteIncorrectCommand();
+			return new CommandResult(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+		}
+
+		for(int i = 0; i < targetIndexes.size(); i++){
+
+			ReadOnlyTask taskToDelete = lastShownList.get(targetIndexes.get(i) - i - 1);
+
+			try {
+				model.saveToPrevLists();
+				model.deleteTask(taskToDelete);
+			} catch (TaskNotFoundException pnfe) {
+				assert false : "The target task cannot be missing";
+			}
+		}
+		return new CommandResult(String.format(MESSAGE_DELETE_TASK_SUCCESS, targetIndexes));         
+	}          
+}
 
 
