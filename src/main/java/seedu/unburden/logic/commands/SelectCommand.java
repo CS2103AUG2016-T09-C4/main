e9a79bbf2904346d400a41b8b2f14378e18be596
Yang Luhua <e0012658@u.nus.edu>
2016-11-06 22:27:49 +0800

    Select command

--- src/main/java/seedu/unburden/logic/commands/SelectCommand.java
+++ src/main/java/seedu/unburden/logic/commands/SelectCommand.java
@@ -5,39 +5,41 @@ import seedu.unburden.commons.core.UnmodifiableObservableList;
 import seedu.unburden.commons.events.ui.JumpToListRequestEvent;
 import seedu.unburden.model.task.ReadOnlyTask;
 
-
 /**
- * Selects a person identified using it's last displayed index from the address book.
+ * Selects a task identified using it's last displayed index from the address book.
+ * It will also show the details of the task selected, especially
+ * task descriptions that may not be able to show on the taskList panel
+ * @@author A0147986H
  */
 public class SelectCommand extends Command {
 
-    public final int targetIndex;
+    public final int taskIndex;
 
     public static final String COMMAND_WORD = "select";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Selects the person identified by the index number used in the last person listing.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
+            + ": Selects the task identified by the index number used in the last task listing.\n"
+            + "Parameters: INDEX (must be a positive integer that does not exceed the maximum number of tasks)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_SELECT_PERSON_SUCCESS = "Selected Task: %1$s";
+    public static final String MESSAGE_SELECT_TASK_SUCCESS = "Selected Task: %1$s";
 
-    public SelectCommand(int targetIndex) {
-        this.targetIndex = targetIndex;
+    public SelectCommand(int taskIndex) {
+        this.taskIndex = taskIndex;
     }
 
+    
     @Override
     public CommandResult execute() {
 
         UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
 
-        if (lastShownList.size() < targetIndex) {
+        if (lastShownList.size() < taskIndex) {
             indicateAttemptToExecuteIncorrectCommand();
             return new CommandResult(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
         }
 
-        EventsCenter.getInstance().post(new JumpToListRequestEvent(targetIndex - 1));
-        return new CommandResult(String.format(MESSAGE_SELECT_PERSON_SUCCESS, targetIndex));
-
+        EventsCenter.getInstance().post(new JumpToListRequestEvent(taskIndex - 1));
+        return new CommandResult(String.format(MESSAGE_SELECT_TASK_SUCCESS, lastShownList.get(taskIndex-1)));
     }
 }
